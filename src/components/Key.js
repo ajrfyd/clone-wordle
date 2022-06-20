887e03616c722745572026da90eb0aedf1baefef
ajrfyd <ajrfyd@naver.com>
2022-06-20 23:22:22 +0900

    Update: Temp

--- src/components/Key.js
+++ src/components/Key.js
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useContext } from "react";
+import { AppContext } from "../App";
 
 const Key = ({ keyVal, bigKey }) => {
+  const { onSelectLetter, onDelete, onEnter } = useContext(AppContext);
+
+
+  const selectLetter = () => {
+    if(keyVal === 'Enter') {
+      onEnter();
+    } else if(keyVal === 'Delete') {
+      onDelete();
+    } else {
+      onSelectLetter(keyVal);
+    }
+  }
 
   return (
-    <div className='key' id={bigKey ? 'big' : 'disabled'}>
+    <div 
+      className='key' 
+      id={bigKey ? 'big' : 'disabled'}
+      onClick={selectLetter}
+    >
       {keyVal}
     </div>
   )
--- src/components/Letter.js
+++ src/components/Letter.js
@@ -2,11 +2,19 @@ import React, { useContext } from "react";
 import { AppContext } from "../App";
 
 const Letter = ({ letterPos, attemptVal }) => {
-  const { board } = useContext(AppContext);
+  const { board, correctWord, currAttempt } = useContext(AppContext);
+  // [row][col];
   const letter = board[attemptVal][letterPos];
 
+  const correct = correctWord[letterPos] === letter;
+  const almost = !correct && letter !== '' && correctWord.toUpperCase().includes(letter);
+
+  // 단어의 상태 
+  // 바로바로 확인 되는 것이 아닌 엔터로 row 값이 증가 했을 때 결과를 확인할 수 있다.
+  const letterState = currAttempt.attempt > attemptVal && (correct ? 'correct' : almost ? 'almost' : 'error');
+
   return (
-    <div className='letter' id=''>
+    <div className='letter' id={letterState || ''}>
       { letter }
     </div>
   )
